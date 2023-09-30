1ccbfb3c410380b8668a10f9aba67c2b70eaf546
João Barbosa <contato.jsbarbosa@gmail.com>
2023-09-29 21:24:14 -0300

    Request Rest - Clients page, Client update, Client by id

--- src/main/java/br/com/joaosbarbosa/trabalho/Resource/ClientResource.java
+++ src/main/java/br/com/joaosbarbosa/trabalho/Resource/ClientResource.java
@@ -3,12 +3,17 @@ package br.com.joaosbarbosa.trabalho.Resource;
 
 import br.com.joaosbarbosa.trabalho.Service.ClientService;
 import br.com.joaosbarbosa.trabalho.dto.ClientDTO;
+import br.com.joaosbarbosa.trabalho.entity.Client;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/clients")
@@ -21,5 +26,32 @@ public class ClientResource {
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAllPages(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPages", defaultValue = "5") Integer linesPerPages,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction)
+    {
+        PageRequest pageRequest = PageRequest.of(page,linesPerPages, Sort.Direction.valueOf(direction), orderBy);
+        Page<ClientDTO> list = clientService.findAllPages(pageRequest);
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+        ClientDTO clientDTO = clientService.searchById(id);
+
+        return ResponseEntity.ok().body(clientDTO);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@RequestBody ClientDTO dto, @PathVariable Long id){
+        dto = clientService.updateClient(dto, id);
+
+        return ResponseEntity.ok().body(dto);
     }
 }
--- src/main/java/br/com/joaosbarbosa/trabalho/Service/ClientService.java
+++ src/main/java/br/com/joaosbarbosa/trabalho/Service/ClientService.java
@@ -3,8 +3,14 @@ package br.com.joaosbarbosa.trabalho.Service;
 import br.com.joaosbarbosa.trabalho.dto.ClientDTO;
 import br.com.joaosbarbosa.trabalho.entity.Client;
 import br.com.joaosbarbosa.trabalho.repository.ClientRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -17,6 +23,37 @@ public class ClientService {
 
         entity = clientRepository.save(entity);
         return new ClientDTO(entity);
+    }
+
+
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAllPages(PageRequest pageRequest) {
+        Page<Client> clientPage = clientRepository.findAll(pageRequest);
+
+        return clientPage.map(client -> new ClientDTO(client));
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO searchById(Long id) {
+        Optional<Client> clientOptional = clientRepository.findById(id);
+
+        Client client = clientOptional.orElseThrow(()-> new RuntimeException("Erro ao localizar id"));
+        return new ClientDTO(client);
+    }
+
+    @Transactional
+    public ClientDTO updateClient(ClientDTO dto, Long id) {
+        try{
+            Client client = clientRepository.getOne(id);
+            copyDtoToEntity(client,dto);
+            client = clientRepository.save(client);
+
+            return new ClientDTO(client);
+
+        }catch (Exception e){
+            throw new EntityNotFoundException("Erro ao localizar o ID"+id);
+        }
     }
 
     private void copyDtoToEntity(Client entity, ClientDTO dto){
